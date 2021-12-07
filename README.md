0edc2d9e2c9c79aa96ea48bcb88db499a33bd038
eb35 <roberthenry82@hotmail.com>
2021-12-07 03:21:07 -0600

    after express was working, not mongodb is working

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Mongo (is just pawn in game of life)
+
+Beginning with using `docker run` commands to build containers. See below.
+
+#1 Create the network
+```
+docker run -d
+	-e ME_CONFIG_MONGODB_ADMINUSERNAME=admin
+	-e ME_CONFIG_MONGODB_ADMINPASSWORD=password
+	--net mongo-network
+```
+
+#2 Start the MongoDB container (to store the data)
+```
+docker run -p 27017:27017 -d
+	-e MONGO_INITDB_ROOT_USERNAME=admin
+	-e MONGO_INITDB_ROOT_PASSWORD=password
+	--name mongodb
+	--net mongo-network
+	mongo
+```
+
+#3 Start the MongoExpress container (to view the data)
+```
+docker run -d
+	-p 8081:8081
+	-e ME_CONFIG_MONGODB_SERVER="mongodb"
+	-e ME_CONFIG_MONGODB_ADMINUSERNAME=admin
+	-e ME_CONFIG_MONGODB_ADMINPASSWORD=password
+	--net mongo-network
+	--name mongoexpress
+	mongo-express
+```
+
+Next steps will be simplifying my container creation
--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
 var app = express();
 
@@ -11,6 +12,54 @@ app.use(bodyParser.json());
 
 app.get(`/`, (req, res) => {
 	res.sendFile(path.join(__dirname, `index.html`));
+});
+
+app.get(`/profile-picture`, (req, res) => {
+	var img = fs.readFileSync(`C:/robcode/Docker/mongopawn/app/images/profile-1.jpg`);
+	res.writeHead(200, { 'Content-Type': 'image/jpg' });
+	res.end(img, 'binary');
+});
+
+app.get(`/get-profile`, (req, res) => {
+	var response = {};
+	
+	MongoClient.connect('mongodb://admin:password@localhost:27017', (err, client) => {
+		if (err) throw err;
+		
+		var db = client.db('user-account');
+		var query = { userid: 1 };
+		
+		db.collection('users').findOne(query, (err, result) => {
+			if (err) throw err;
+			response = result;
+			client.close();
+
+			res.send(response ? response : {});
+		});
+	});
+});
+
+app.post(`/update-profile`, (req, res) => {
+	var userObj = req.body;
+	var response = res;
+
+	console.log("connecting to the db...");
+	
+	MongoClient.connect('mongodb://admin:password@localhost:27017', (err, client) => {
+		if (err) throw err;
+
+		var db = client.db('user-account');
+		userObj['userid'] = 1;
+
+		var myquery = { userid: 1 };
+		var newvalues = { $set: userObj };
+
+		db.collection('users').updateOne(myquery, newvalues, { upsert: true }, (err, res) => {
+			if (err) throw err;
+			client.close();
+		});
+	});
+	res.send(userObj);
 });
 
 app.listen(3000, () => {
